7f098e01ddc8d658d9ddf1efc36d9d4530dcc3d2
Matt Richardson <matt.richardson@msrconsults.com>
2015-05-05 21:39:36 -0600

    Committing revisions associated with Angular test.  Lots of functional changes, going to add web service support next.

--- test/app/shared/main-config.js
+++ test/app/shared/main-config.js
@@ -3,6 +3,7 @@
 	.config(['$routeProvider', function($routeProvider){
 		$routeProvider
 		.when('/CRDS',{templateUrl:'views/crds.html'})
-		.when('/PAS',{templateUrl:'views/pas.html'});
+		.when('/PAS',{templateUrl:'views/pas.html'})
+		.when('/O3',{templateUrl:'views/ozone.html'});
 	}]);
 })();
