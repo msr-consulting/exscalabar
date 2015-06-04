d4a78b8823ed8ae8f914db6f97239d659d30051e
Matt Richardson <matt.richardson@msrconsults.com>
2015-06-04 17:36:28 -0600

    Valid and tested changes to the cvt service and the pas controller/view. Resolved issue with what appeared to be race conditions but was simply a validity problem (as defined by the range).

--- test/app/shared/cvt-service.js
+++ test/app/shared/cvt-service.js
@@ -4,21 +4,22 @@
 
 (function() {
 	angular.module('main')
-	.factory('cvt', 
-	function() {
+	.factory('cvt', function() {
 		
-		return {
-			speaker: {"vrange": 5,
+		var cvt = {};
+		
+		var _speaker = {"vrange": 5,
 						"voffset": 0,
 						"f0": 1350,
 						"df": 100,
-						"pos": true},
-			getPasSpkCtl: function(){return this.speaker;},
-			setPasSpkCtl: function(spk){
-				this.speaker = spk;
-				}
-			
-		};
-	});
+						"pos": true};
+						
+		cvt.getPasSpkCtl = function(){return _speaker;};
+		cvt.setPasSpkCtl = function(spk){
+			_speaker = spk;
+			};
+		
+		return cvt;
 
+	});
 	})();
