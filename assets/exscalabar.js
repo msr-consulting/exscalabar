0ba29e65dec9c79b4021fa4e34113552bfd0bed1
msrconsultingllc <matt.richardson@msrconsults.com>
2015-07-31 11:48:41 -0600

    Working on the CRD UI page.  Currently broken.

--- assets/exscalabar.js
+++ assets/exscalabar.js
@@ -737,23 +737,30 @@
 	function($scope, net, $http, cvt, Data) {
 
 		// Lasers have three inputs
-		var laserInput = function(_rate, _DC, _k) {
+		var laserInput = function(_rate, _DC, _k, enabled, ID) {
 			this.rate = _rate;
 			this.DC = _DC;
 			this.k = _k;
+			this.en = enabled
+			this.id = ID;
 		};
-		$scope.blue = new laserInput(cvt.crd.fblue, cvt.crd.dcblue, cvt.crd.kblue);
-		$scope.red = new laserInput(cvt.crd.fred, cvt.crd.dcred, cvt.crd.kred);
-		$scope.pmt = cvt.crd.kpmt;
-		$scope.benabled = cvt.crd.eblue;
-		$scope.renabled = cvt.crd.ered;
+		/* Variable for laser control binding; first element is related to blue,
+		 * second to red.
+		 */
+		$scope.laser_ctl = [
+			new laserInput(cvt.crd.fblue, cvt.crd.dcblue, cvt.crd.kblue, cvt.crd.eblue, "Blue Laser"),
+			new laserInput(cvt.crd.fred, cvt.crd.dcred, cvt.crd.kred, cvt.crd.ered, "Red Laser")];
 
+		$scope.pmt = cvt.crd.kpmt;
+
+		// TODO: Implement enabled functionality
 		$scope.setbEnable = function() {
 			$scope.benabled = !$scope.benabled;
 			cvt.crd.eblue = $scope.benabled;
 			//$http.get(net.address() + 'PAS_CMD/SpkSw?SpkSw=' + val);
 		};
 
+		// TODO: Implement enabled functionality
 		$scope.setrEnable = function() {
 			$scope.renabled = !$scope.renabled;
 			cvt.crd.ered = $scope.renabled;
