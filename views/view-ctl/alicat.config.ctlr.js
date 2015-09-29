134426d8661a1be933eded652da8c4ed2dfee13c
mtat76 <nosdra>
2015-09-29 17:21:58 -0600

    Working primarily with configuration page.

---
+++ views/view-ctl/alicat.config.ctlr.js
@@ -0,0 +1,29 @@
+(function() {
+    angular.module('main')
+      .controller('mrAlicatConfigCtlr', ['$scope', function($scope) {
+          
+          
+          /* This will contain the template for the list of Alicat
+           * devices.
+           */
+          function ListEntry(addr, id){
+              this.address = addr;
+              this.id = id;
+          };
+          
+          $scope.entry = new ListEntry("A","default");
+          
+          /* Store devices here */
+          $scope.devices = [];
+          
+          $scope.addDevice = function(){
+              $scope.devices.push(new ListEntry($scope.entry.address, $scope.entry.id));
+              
+          };
+          
+          $scope.rmDevice = function(){
+              // Not implemented
+          };
+          
+      }]);
+})()
